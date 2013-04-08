a19d71e8ff02d8c2767fda1d5fcf901f214bceae
Jesper Dramsch <jesper.dramsch@zmaw.de>
2013-04-08 23:16:31 +0300

    Apertur eine Seite korrekt

--- CO_kirch.m
+++ CO_kirch.m
@@ -1,3 +1,7 @@
+                                                                     
+                                                                     
+                                                                     
+                                             
 %{
 
     CO_kirch.m - Constant offset Kirchhoff migration.
@@ -23,7 +27,7 @@ function [Kirchhoff, Skala] = CO_kirch(data, v, h, dt, dcmp, half_aper)
 Kirchhoff = zeros(nt,nx);
 
 %% Schleife ueber CMPs
-for i_cmp=-nx:nx                              % Indices benachbarter CMPs
+for i_cmp=1:nx                              % Indices benachbarter CMPs
     cmp = dcmp*i_cmp;                         % CMP-Abstand der Indices
     
 %% Schleife ueber Laufzeitsamples
@@ -42,18 +46,20 @@ for i_cmp=-nx:nx                              % Indices benachbarter CMPs
         % aus dem Paper von Zhang Y. (2000)
 
         % Aperturgrenzen
-        if(abs(i_cmp)<(nx-half_aper) && abs(i_cmp)>(nx-half_aper))
+        if(i_cmp>(1+half_aper) && i_cmp<(nx-half_aper))
             bound_l = max(floor(i_cmp-half_aper),1);
             bound_r = min(floor(i_cmp+half_aper),nx);
         else
-            bound_l = max(floor(1-i_cmp),  1);           % Aperturgrenzen
+            bound_l = max(floor(1-i_cmp),  1);
             bound_r = min(floor(nx-i_cmp), nx);
         end
-
+        
 %% Schleife ueber Apertur        
         for i_aper=bound_l:bound_r
-            Kirchhoff(i_t,i_aper)=Kirchhoff(i_t,i_aper)+data(i_t,i_cmp+i_aper)*amp;
+%            Kirchhoff(i_t,i_aper)=Kirchhoff(i_t,i_aper)+data(it,i_cmp+i_aper)*amp;
+            Kirchhoff(i_t,i_cmp)=Kirchhoff(i_t,i_cmp)+data(it,i_cmp+i_aper)*amp;
         end
+
     end
 end
 Skala = sqrt((h/(v)).^2+((0:nt-1)'*dt).^2)*v;          % Tiefenskalierung
